592dba301ed3d416a24525cd5286e2afcff943a9
Jean Baptiste <shumbushojean1@gmail.com>
2025-08-26 11:40:59 +0200

    Stop tracking _api folder and ignore it

--- _api/core/Response.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Response {
-    public static function json($data, $code = 200) {
-        http_response_code($code);
-        header("Content-Type: application/json");
-        echo json_encode($data);
-    }
-}
--- _api/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once 'secure/security.php';
-include_once 'secure/cors.php';
-require_once 'routes/api.php';
-
-?>
